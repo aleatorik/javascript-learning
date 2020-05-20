204865dbb27685bfda3c20bb8f7dc9abec73a7ba
aleatorik <vates0328@gmail.com>
2020-05-21 02:55:03 +0900

    update: Class vs Object and OOP Class

--- class.js
+++ class.js
@@ -5,3 +5,46 @@
 // JavaScript classes
 // - introduced in ES6
 // - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    // fields
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods
+  speak() {
+    console.log(`${this.name}: hello!`);
+  }
+}
+
+const alex = new Person("alex", 23);
+console.log(alex.name);
+console.log(alex.age);
+alex.speak();
+
+//  2. Getter and Setters
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    // if (value < 0) {
+    //   throw Error("age can not be negative");
+    // }
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User("Steve", "Job", -1);
+console.log(user1.age);
