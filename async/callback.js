ed0748d4d996e61a6a06c748ad0a9b7fe9c30ec9
aleatorik <vates0328@gmail.com>
2020-06-13 23:07:38 +0900

    update: Callback(JavaScript ES6)

--- async/callback.js
+++ async/callback.js
@@ -32,6 +32,7 @@ function printWithDelay(print, timeout) {
 printWithDelay(() => console.log("async callback"), 2000);
 
 // Callback Hell example
+
 class UserStorage {
   loginUser(id, password, onSuccess, onError) {
     setTimeout(() => {
@@ -45,4 +46,38 @@ class UserStorage {
       }
     }, 2000);
   }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === "daniel") {
+        onSuccess({ name: "daniel", role: "admin" });
+      } else {
+        onError(new Error("no access"));
+      }
+    }, 1000);
+  }
 }
+
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+userStorage.loginUser(
+  id,
+  password,
+  (user) => {
+    userStorage.getRoles(
+      user,
+      (userWithRole) => {
+        alert(
+          `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  (error) => {
+    console.log(error);
+  }
+);
